88052c6c34b4e28c594f6ec93da096ff2ae63021
SkanderHannachi <skander.hannachi@esprit.tn>
2017-08-16 11:10:33 +0200

    up README

--- NN_histogram.py
+++ NN_histogram.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
-"""Classifying histograms with a Neural Network.  """
+# coding: utf-8
+
+
+"""Classifying histograms with a Neural Network.  
+
+to explore : http://www.chioka.in/why-is-keras-running-so-slow/
+
+
+"""
 
 
 TEST_SIZE           =    10 
 POURCENTAGE_TRAIN   =    0.15
+
+HIDDEN_LAYER_SIZE = 55 
+NB_FEATURES       = 111
+
 
 from sklearn import cross_validation 
 import matplotlib.pyplot as plt
@@ -40,7 +52,7 @@ class NeuralNetwork:
 				
 	def build(self): 
 		Network = Sequential() 
-		Network.add(Dense(output_dim = 55, init='uniform', activation='relu', input_dim=111)) #first hidden layer
+		Network.add(Dense(output_dim = HIDDEN_LAYER_SIZE, init='uniform', activation='relu', input_dim=NB_FEATURES)) #first hidden layer
 		Network.add(Dense(output_dim = 1, init='uniform', activation='sigmoid')) #output neuron 
 		Network.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy']) #compile NN
 			#optimizer : how to tune wieghts
@@ -49,7 +61,7 @@ class NeuralNetwork:
 		return Network
 	
 	def fit_to_training(self):
-		self.NN.fit(self.X_train, self.Y_train, batch_size = 10, nb_epoch = 100 )
+		self.NN.fit(self.X_train, self.Y_train, batch_size = 10, nb_epoch = 10000 )
 		#self.NN.predict(np.transpose(self.simulate()))
 		self.visualize()
 
@@ -58,7 +70,7 @@ class NeuralNetwork:
 	
 	def visualize(self):
 		fig = plt.figure(figsize=(12, 12))
-		self.draw_neural_net(fig.gca(), .1, .9, .1, .9, [111, 55, 1])
+		self.draw_neural_net(fig.gca(), .1, .9, .1, .9, [20, 7, 1])
 		plt.show()
 
 	def draw_neural_net(self, ax, left, right, bottom, top, layer_sizes):
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # NN for histograms
 
 
-![screenshot_20170816_014543](https://user-images.githubusercontent.com/21022345/29341456-b1a6720a-8224-11e7-8d85-eb6d4b41ff47.png)
+![nn](https://user-images.githubusercontent.com/21022345/29356110-749fed92-8273-11e7-8a9e-1ab0592180c2.png)
+
 
 Input features size = 111
 Hidden Layers = 1 X 55
